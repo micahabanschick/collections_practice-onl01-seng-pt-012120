30c25d605cdbc1a2c8697a84dd4faed1c813261c
Micah Banschick <micha.ban@gmail.com>
2020-01-23 21:47:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,18 +1,26 @@
-def sort_array_asc
-    
-def sort_array_desc
+def sort_array_asc(arr)
+end 
   
-def sort_array_char_count
- 
-def swap_elements
-  
-def reverse_array
-  reverse the order of an array of integers (FAILED - 5)
-def kesha_maker
-  taking an array as an input, change the 3rd character of each element to a dollar sign. (FAILED - 6)
-def find_a
-  find all words that begin with "a" in the following array (FAILED - 7)
-def sum_array
-  sum all the numbers in the following array (FAILED - 8)
-def add_s
-  Add an "s" to each word in the array except for the 2nd element in the array (FAILED - 9)
+def sort_array_desc(arr)
+end 
+
+def sort_array_char_count(arr)
+end 
+
+def swap_elements(arr)
+end 
+
+def reverse_array(arr)
+end 
+
+def kesha_maker(arr)
+end 
+
+def find_a(arr)
+end 
+
+def sum_array(arr)
+end 
+
+def add_s(arr)
+end   
