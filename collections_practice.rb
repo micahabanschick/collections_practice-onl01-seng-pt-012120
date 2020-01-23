a78a301ae7ea31d5a5dedba3659342283ae8ba3f
Micah Banschick <micha.ban@gmail.com>
2020-01-23 21:47:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,26 +1,35 @@
 def sort_array_asc(arr)
+  (arr)
 end 
   
 def sort_array_desc(arr)
+  (arr)
 end 
 
 def sort_array_char_count(arr)
+  (arr)
 end 
 
 def swap_elements(arr)
+  (arr)
 end 
 
 def reverse_array(arr)
+  (arr)
 end 
 
 def kesha_maker(arr)
+  (arr)
 end 
 
 def find_a(arr)
+  (arr)
 end 
 
 def sum_array(arr)
+  (arr)
 end 
 
 def add_s(arr)
+  (arr)
 end   
